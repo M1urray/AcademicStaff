d0dff8551d4c0fb65fabbc546db22c929b5b8f16
M1urray <rknjonjo@gmail.com>
2023-08-07 09:37:38 +0300

    Example deletion

--- Latest Staff Portal/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Latest_Staff_Portal.MvcApplication" Language="C#" %>
--- Latest Staff Portal/SiteJS/DashboardJS.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-    $('.shadow-none').on('shown.bs.collapse', function () {
-        $(this).find('.fa-arrow-down').removeClass('fa-arrow-down').addClass('fa-arrow-up');
-    }).on('hidden.bs.collapse', function () {
-        $(this).find('.fa-arrow-up').removeClass('fa-arrow-up').addClass('fa-arrow-down');
-    });
-});
