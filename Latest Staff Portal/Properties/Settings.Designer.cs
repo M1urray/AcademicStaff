69c4162e1898e2f824625f6e82b75561c75dd184
M1urray <rknjonjo@gmail.com>
2024-01-17 14:56:54 +0300

    Modify web reference

--- Latest Staff Portal/Properties/Settings.Designer.cs
+++ Latest Staff Portal/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Latest_Staff_Portal.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace Latest_Staff_Portal.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://192.168.7.153:7047/ERP/WS/DAYSTAR%20UNI/Codeunit/WebPortal")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://108.181.199.151:9740/ANIU/WS/ANIU/Codeunit/WebPortal")]
         public string Latest_Staff_Portal_NAVWS_WebPortal {
             get {
                 return ((string)(this["Latest_Staff_Portal_NAVWS_WebPortal"]));
